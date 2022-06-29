a61406653fb305de1cfe508f3ebf18469d562803
JoenTheWizard <75371093+JoenTheWizard@users.noreply.github.com>
2022-06-29 23:57:14 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 let MenubarElem = document.getElementById("menubarID");
 
 //This function will open the menu bar (from button click)
-function openMenubar()
-{
+function openMenubar() {
 	MenubarElem.style.width = "250px";
 }
 
 //This function will close the menu bar (from close menu button)
-function closeMenubar()
-{
+function closeMenubar() {
 	MenubarElem.style.width = "0px";
 }
-
-//This function returns the date and sets it to the <p> element at the footer
-function getDate()
-{
-	var date = new Date();
-	document.getElementById("dateNow").innerHTML = `Created by Joe Nicola<br>${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-}
-getDate();
